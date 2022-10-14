57c0cc2fbf415e1bb550eb321cded4954cff0902
Brad Sickles <brad.sickles@gmail.com>
2022-10-14 08:49:10 -0400

    Fix domain and security group

--- domain.tf
+++ domain.tf
@@ -1,7 +1,7 @@
 data "aws_elasticsearch_domain" "this" {
-  domain_name = "my-domain-name"
+  domain_name = var.domain_name
 }
 
 locals {
-  db_security_group_id = try(data.aws_elasticsearch_domain.this.vpc_options[0].security_group_ids[0], "")
+  db_security_group_id = try(tolist(data.aws_elasticsearch_domain.this.vpc_options[0].security_group_ids)[0], "")
 }
